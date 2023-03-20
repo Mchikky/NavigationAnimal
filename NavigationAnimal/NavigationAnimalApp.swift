87082b1716e30a65a4b3c2bf7ddcbe61de944345
Chikaodili Deng <mchikky@yahoo.com>
2023-03-20 11:34:14 +0100

    Initial Commit

---
+++ NavigationAnimal/NavigationAnimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAnimalApp.swift
+//  NavigationAnimal
+//
+//  Created by Chikaodili Deng on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAnimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
